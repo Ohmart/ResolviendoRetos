29b0b270111ffae927458939269d4b17270cffe9
ooht_ <oohtestudiante@outlook.com>
2023-04-25 22:58:28 -0600

    Reto #3

---
+++ 003Primo.py
@@ -0,0 +1,35 @@
+'''
+ * Reto #3
+ * ¿ES UN NÚMERO PRIMO?
+ * Fecha publicación enunciado: 17/01/22
+ * Fecha publicación resolución: 24/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe un programa que se encargue de
+ * comprobar si un número es o no primo.
+ * Hecho esto, imprime los números primos entre 1 y 100.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ '''
+
+# 10:33
+# 10:41
+# 7 minutos
+
+def es_primo(num):
+    if num < 2: return False 
+    mitad = num // 2 + 1
+    for i in range (2, mitad):
+        if num % i == 0: return False
+    return True
+
+#for x in range(1, 101):
+#   print(str(x), "¿Es un número primo?",es_primo(x))
+for x in range(1, 101):
+   if es_primo(x):
+       print(x)
